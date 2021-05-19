dmfrodrigues/GraphViewerCpp

commit 0e51959249b676d456a7c51310bb699f21e5c999

Fixed #12 (setCenter() crashing the window)

src/graphviewer.cpp
@@ -65,7 +65,22 @@ void GraphViewer::createWindow(unsigned int width, unsigned int height){
     this->width  = width;
     this->height = height;
 
+    // Create window
+    ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    GraphViewer::createWindowMutex.lock();
+    window = new RenderWindow(VideoMode(this->width, this->height), "GraphViewer", Style::Default, settings);
+    GraphViewer::createWindowMutex.unlock();
+
+    // Create views
+    view = new View(window->getDefaultView());
+    debug_view = new View(window->getDefaultView());
+
+    // Recalculate view
+    recalculateView();
+
     windowOpen = true;
+    window->setActive(false);
     main_thread = new thread(&GraphViewer::run, this);
 }
 
@@ -226,20 +241,11 @@ void GraphViewer::updateZip(){
 }
 
 void GraphViewer::run(){
-    ContextSettings settings;
-    settings.antialiasingLevel = 8;
-    GraphViewer::createWindowMutex.lock();
-    window = new RenderWindow(VideoMode(this->width, this->height), "GraphViewer", Style::Default, settings);
-    GraphViewer::createWindowMutex.unlock();
-
-    view = new View(window->getDefaultView());
-    debug_view = new View(window->getDefaultView());
+    window->setActive(true);
 
     bool isLeftClickPressed = false;
     Vector2f centerInitial;
     Vector2f posMouseInitial;
-
-    recalculateView();
 
     while (window->isOpen()){
         Event event;

commit 7a404295946e993a445ceb4537c990c520d5e247

Fixed issue introduced in 0e51959 (Windows: window not responding)

include/graphviewer.h
@@ -725,6 +725,7 @@ private:
     sf::View *debug_view = nullptr;             ///< @brief Debug view, to draw debug information.
     std::thread *main_thread = nullptr;         ///< @brief Main thread.
     bool windowOpen = false;
+    std::mutex windowInitialization;            ///< @brief Mutex: createWindow only returns once window is created.
 
     bool enabledNodes     = true;               ///< @brief Node drawing enabled.
     bool enabledNodesText = true;               ///< @brief Node text drawing enabled.

src/graphviewer.cpp
@@ -65,23 +65,10 @@ void GraphViewer::createWindow(unsigned int width, unsigned int height){
     this->width  = width;
     this->height = height;
 
-    // Create window
-    ContextSettings settings;
-    settings.antialiasingLevel = 8;
-    GraphViewer::createWindowMutex.lock();
-    window = new RenderWindow(VideoMode(this->width, this->height), "GraphViewer", Style::Default, settings);
-    GraphViewer::createWindowMutex.unlock();
-
-    // Create views
-    view = new View(window->getDefaultView());
-    debug_view = new View(window->getDefaultView());
-
-    // Recalculate view
-    recalculateView();
-
-    windowOpen = true;
-    window->setActive(false);
+    windowInitialization.lock();
     main_thread = new thread(&GraphViewer::run, this);
+    windowInitialization.lock();
+    windowInitialization.unlock();
 }
 
 void GraphViewer::closeWindow(){
@@ -241,11 +228,23 @@ void GraphViewer::updateZip(){
 }
 
 void GraphViewer::run(){
-    window->setActive(true);
+    ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    GraphViewer::createWindowMutex.lock();
+    window = new RenderWindow(VideoMode(this->width, this->height), "GraphViewer", Style::Default, settings);
+    GraphViewer::createWindowMutex.unlock();
+
+    view = new View(window->getDefaultView());
+    debug_view = new View(window->getDefaultView());
 
     bool isLeftClickPressed = false;
     Vector2f centerInitial;
     Vector2f posMouseInitial;
+
+    recalculateView();
+    windowOpen = true;
+
+    windowInitialization.unlock();
 
     while (window->isOpen()){
         Event event;
@@ -354,10 +353,10 @@ void GraphViewer::draw() {
 
 void GraphViewer::drawDebug(){
     window->setView(*debug_view);
-    
+
     string debugInfo;
     debugInfo += "FPS: " + to_string(int(fps_monitor.getFPS())) + "\n";
-    
+
     if(debugInfo[debugInfo.size()-1] == '\n')
         debugInfo = debugInfo.substr(0, debugInfo.size()-1);
     debug_text.setString(debugInfo);